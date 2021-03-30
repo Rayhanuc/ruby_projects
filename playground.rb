ff8df254f9382000ef90c16d952f17dbe050a125
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2021-03-30 12:19:06 +0600

    Initial commit

---
+++ playground.rb
@@ -0,0 +1,24 @@
+puts "hello world!"
+
+# Hello World Program in Ruby
+puts "Hello World!";
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+    
+end
+
+say_hello "Hello World Ruby is great!"
+
+
+# String concatenation
+"My"
+# String interpolation
+
+# Methods, how to find them
+
+# Common Methods
+
+# Variable assignment
+
+# Escaping
