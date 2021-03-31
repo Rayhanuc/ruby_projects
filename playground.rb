d9a3d372b56e41acec54a653098a28877b525bb7
[Rayanuc] <30935802+Rayhanuc@users.noreply.github.com>
2021-03-31 08:06:52 +0600

    4. Working with Strings part 1
    4. Working with Strings part 1
    
    Half video code practice done

--- playground.rb
+++ playground.rb
@@ -23,7 +23,26 @@ puts first_name + " " + middle_name + " " + last_name
 
 # String interpolation
 puts "My first name is #{first_name} middle name is #{middle_name} and last name is #{last_name}"
+
+full_name = "#{first_name} #{middle_name} #{last_name}"
+p full_name
 # Methods, how to find them
+full_name.class
+"Rayhan".class
+10.class
+10.0.class
+first_name.methods
+10.to_s
+10.to_s.class
+full_name.length
+full_name.reverse
+full_name.capitalize
+full_name.empty?
+"".empty?
+"".nil?
+nil.nil?
+sentence = "Welcome to the jungle"
+sentence.sub("the jungle", "utopia")
 
 # Common Methods
 
